55059e55545be2b157cf13a84a5453c2d0f01684
Juarez Sant'Ana Vieira da Cruz <juarezsantana0897@gmail.com>
2023-10-05 15:25:54 -0300

    Arrumei o título e coloquei a seção de footer.

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import AdviceCard from "./Components/AdviceCard"
+import Attribution from "./Components/Attribution"
 
 
 function App() {
@@ -6,6 +7,7 @@ function App() {
   return (
     <div className="w-full min-h-screen bg-mainBg flex items-center justify-center">
       <AdviceCard />
+      <Attribution />
     </div>
   )
 }
---
+++ src/Components/Attribution/index.tsx
@@ -0,0 +1,8 @@
+export default function Attribution() {
+    return (
+        <footer className="w-full p-0 md:p-3 absolute inset-x-0 bottom-0 text-center text-txtQuotes">
+            Challenge by <a href="https://www.frontendmentor.io?ref=challenge" target="_blank" className="font-semibold hover:underline text-neon"> Frontend Mentor</a>.
+            Coded by <a href="https://github.com/oJuarezCruz" className="font-semibold hover:underline text-neon">Juarez Cruz.</a>
+        </footer>
+    )
+}
